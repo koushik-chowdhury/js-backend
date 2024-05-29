87d1f58d04591b5695d64c326014ea7c834e2d7e
Koushik Chowdhury <csakoushik@gmail.com>
2024-05-29 23:32:09 +0530

    modify server

--- 01_web_server/server-node.js
+++ 01_web_server/server-node.js
@@ -4,15 +4,15 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.statusCode = 300;
+    res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Hello koushik, Our server is running");
   } else if (req.url === "/name") {
-    res.statusCode = 300;
+    res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("koushik chowdhury");
   } else {
-    res.statusCode = 300;
+    res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("404 Not found");
   }
