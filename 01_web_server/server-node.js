c314df52bc4461463934c2aa96519d410c1ea986
Koushik Chowdhury <csakoushik@gmail.com>
2024-06-08 21:54:22 +0530

    add a new rout

--- 01_web_server/server-node.js
+++ 01_web_server/server-node.js
@@ -6,18 +6,22 @@ const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
+    console.log(req.headers);
     res.end("Hello koushik, Our server is running");
   } else if (req.url === "/name") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("koushik chowdhury");
+  } else if (req.url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello, my name is koushik chowdhury and i am a web developer.");
   } else {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("404 Not found");
   }
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server is listening at http://${hostname}:${port}`);
 });
